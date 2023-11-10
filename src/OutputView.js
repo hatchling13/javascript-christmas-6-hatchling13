d40dadfd6350e9c9867d37ca82a6b5f4a7e73404
Jeongwook Park <hatchling3713@gmail.com>
2023-11-11 02:40:46 +0900

    feat(OutputView): 안내 문구 출력 기능 작성

--- src/OutputView.js
+++ src/OutputView.js
@@ -1,9 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+
+import CONSTANT from './constant';
+
 const OutputView = {
+  printGreeting() {
+    Console.print(CONSTANT.MESSAGE.GREETING);
+  },
+  /**
+   * @param {string} date
+   */
+  printBenefits(date) {
+    Console.print(`${date}에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n`);
+  },
   printMenu() {
     Console.print('<주문 메뉴>');
-    // ...
   },
-  // ...
 };
 
 export default OutputView;
