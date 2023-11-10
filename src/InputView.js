817fcfdd120e19ac32f215b7bf1f4ac4de21c76f
Jeongwook Park <hatchling3713@gmail.com>
2023-11-11 01:48:46 +0900

    feat(InputView): 잘못된 값 입력 시 해당 부분부터 입력을 다시 받도록 구현

--- src/InputView.js
+++ src/InputView.js
@@ -1,12 +1,20 @@
+import { Console } from '@woowacourse/mission-utils';
+
 import VisitDate from './VisitDate.js';
 
 import CONSTANT from './constant.js';
 
 const InputView = {
   async readDate() {
-    const input = await Console.readLineAsync(CONSTANT.MESSAGE.GREETING);
+    while (true) {
+      try {
+        const input = await Console.readLineAsync(CONSTANT.MESSAGE.INPUT_DATE);
 
-    return new VisitDate(input);
+        return new VisitDate(input);
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
   },
   // ...
 };
