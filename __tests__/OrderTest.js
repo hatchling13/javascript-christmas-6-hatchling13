4615558373923176795f1d4103f67e980bbc680a
Jeongwook Park <hatchling3713@gmail.com>
2023-11-14 02:13:50 +0900

    test(Order): 구현된 도메인 로직에 대한 단위 테스트 추가

--- __tests__/OrderTest.js
+++ __tests__/OrderTest.js
@@ -36,9 +36,52 @@ describe('주문 클래스 테스트', () => {
     }).toThrow('[ERROR]');
   });
 
-  test('할인 전 총주문 금액 계산 정상 동작', () => {
+  test('할인 전 총주문 금액 계산 함수 테스트', () => {
     const order = new Order('해산물파스타-2,레드와인-1,초코케이크-1', date);
 
     expect(order.totalPriceBeforeDiscount()).toBe(145_000);
+  });
+
+  test('증정 이벤트 해당 여부 확인 함수 테스트', () => {
+    const order = new Order('티본스테이크-3', date);
+
+    expect(order.canGetGift()).toBe(true);
+  });
+
+  test('크리스마스 디데이 할인 금액 계산 함수 테스트', () => {
+    const order = new Order('해산물파스타-2,레드와인-1,초코케이크-1', date);
+
+    expect(order.dDayDiscount()).toBe(3_400);
+  });
+
+  test('평일 할인 금액 계산 함수 테스트', () => {
+    const order = new Order('해산물파스타-2,레드와인-1,초코케이크-1', date);
+
+    expect(order.weekdayDiscount()).toBe(2_023);
+  });
+
+  test('주말 할인 금액 계산 함수 테스트', () => {
+    const weekend = new VisitDate('1');
+    const order = new Order('해산물파스타-2,레드와인-1,초코케이크-1', weekend);
+
+    expect(order.weekendDiscount()).toBe(4_046);
+  });
+
+  test('특별 할인 금액 계산 함수 테스트', () => {
+    const order = new Order('해산물파스타-2,레드와인-1,초코케이크-1', date);
+
+    expect(order.specialDiscount()).toBe(1_000);
+  });
+
+  test('총혜택 금액 계산 함수 테스트', () => {
+    const order = new Order('티본스테이크-3,레드와인-1,초코케이크-2', date);
+
+    expect(order.totalDiscountPrice()).toBe(33_446);
+  });
+
+  test('할인 후 예상 결제 금액 계산 함수 테스트', () => {
+    const order = new Order('티본스테이크-3,레드와인-1,초코케이크-2', date);
+
+    expect(order.expectedPayment()).toBe(246_554);
   });
 });
